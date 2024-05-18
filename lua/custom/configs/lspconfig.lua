bffff0075eed6b4f2450effbe0188ed5f1c84f69
Fabio Theophilo <fmarquestheophilo@guidewire.com>
2024-05-18 19:20:58 +0100

    feat: add Prettier and fix Kotlin support

---
+++ lua/custom/configs/lspconfig.lua
@@ -0,0 +1,51 @@
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
+local lspconfig = require "lspconfig"
+
+-- if you just want default config for the servers then put them in a table
+local servers = { "html", "cssls", "clangd" }
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
+
+-- 
+-- lspconfig.pyright.setup { blabla}
+
+lspconfig.kotlin_language_server.setup{
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
+
+local function organize_imports()
+  local params = {
+    command = "_typescript.organizeImports",
+    arguments = {vim.api.nvim_buf_get_name(0)},
+  }
+  vim.lsp.buf.execute_command(params)
+end
+
+lspconfig.tsserver.setup{
+  on_attach = on_attach,
+  capabilities = capabilities,
+  commands = {
+    OrganizeImports = {
+      organize_imports,
+      description = "Organize Imports",
+    }
+  }
+}
+
+lspconfig.eslint.setup {
+  on_attach = function(client, bufnr)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      command = "EslintFixAll",
+    })
+  end,
+  capabilities = capabilities
+}
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -291,6 +291,41 @@ local default_plugins = {
       require('marks').setup()
     end
   },
+
+  {
+    "mhartington/formatter.nvim",
+    event = "VeryLazy",
+    config = function ()
+      require('formatter').setup {
+        -- Enable or disable logging
+        logging = true,
+        -- Set the log level
+        log_level = vim.log.levels.WARN,
+        -- All formatter configurations are opt-in
+        filetype = {
+          -- Formatter configurations for filetype "lua" go here
+          -- and will be executed in order
+          javascript = {
+            require("formatter.filetypes.javascript").prettier
+          },
+          typescript = {
+            require("formatter.filetypes.typescript").prettier
+          },
+          typescriptreact = {
+            require("formatter.filetypes.typescriptreact").prettier
+          },
+
+          -- Use the special "*" filetype for defining formatter configurations on
+          -- any filetype
+          ["*"] = {
+            -- "formatter.filetypes.any" defines default configurations for any
+            -- filetype
+            require("formatter.filetypes.any").remove_trailing_whitespace
+          }
+        }
+      }
+    end
+  }
 }
 
 local config = require("core.utils").load_config()
